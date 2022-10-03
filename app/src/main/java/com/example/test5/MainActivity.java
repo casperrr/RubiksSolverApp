ec82640db14e8474223a69e1a7e82c54c3479ecc
casperrr <casper.sebastien@gmail.com>
2022-10-03 23:33:39 +0100

    christ

--- app/src/main/java/com/example/test5/MainActivity.java
+++ app/src/main/java/com/example/test5/MainActivity.java
@@ -1,15 +1,33 @@
 package com.example.test5;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
 import android.os.Bundle;
-import android.widget.Button;
+import android.os.Vibrator;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Vibrator vib;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        vib = (Vibrator)getSystemService(VIBRATOR_SERVICE);
+
+        AppCompatButton btnAdd = findViewById(R.id.addButton);
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                vib.vibrate(50);
+                //Toast.makeText(MainActivity.this,"add button pressed",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
     }
 }
